55e721d21508accdc4c4584545be9867a7ff03c8
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-05-15 18:24:29 +0200

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# chat-app
+# Chat application
+
+#### 🌐 **[Live show](https://chat-app-26d71.web.app/)**
+
+![preview](./preview.jpg)
+
+---
+
+<p align="center">
+  <a href="https://skillicons.dev">
+    <img src="https://skillicons.dev/icons?i=typescript,react,firebase,nodejs,styledcomponents" />
+  </a>
+</p>
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,45 @@
-import React, {useState } from 'react';
-import {BrowserRouter,Routes,Route, Navigate} from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/Home';
 import Chat from './components/Chat';
 import Register from './components/Register';
 import Login from './components/Login';
-import {onAuthStateChanged } from 'firebase/auth';
+import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './components/firebaseConfig';
 import Loader from './components/Loader';
 
 
 function App() {
-  const [user,setUser] = useState<any>(null)
-  const [isLoading,setIsLoading] = useState<boolean>(true)
+  const [user, setUser] = useState<any>(null)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
 
-  onAuthStateChanged(auth,(currentUser) =>{
+  onAuthStateChanged(auth, (currentUser) => {
     setUser(currentUser)
     setIsLoading(false);
   })
-  
-  if(isLoading){
-    return(
-      <Loader/>
+
+  if (isLoading) {
+    return (
+      <Loader />
     )
   }
 
-  if(!isLoading){
+  if (!isLoading) {
     return (
-    <BrowserRouter>
+      <BrowserRouter>
         <Routes>
-        <Route path='/' element={user ===null ? <Home/> : <Navigate to="/chat"/>}/>
-        <Route path='/register' element={<Register/>}/>
-          <Route path='/login' element={<Login/>}/>
-        <Route path='/chat' element={user !== null? <Chat user={user}/> : <Navigate to="/"/>}/>
+          <Route path='/' element={user === null ? <Home /> : <Navigate to="/chat" />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/chat' element={user !== null ? <Chat user={user} /> : <Navigate to="/" />} />
         </Routes>
-    </BrowserRouter>
-  );
+      </BrowserRouter>
+    );
   }
-  return(
-      <Loader/>
-    )
-  
+  return (
+    <Loader />
+  )
+
 }
 
 export default App;
